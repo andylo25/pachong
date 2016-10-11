961ff3dea492c47aebaecc03279acf690c1d24d8
andylo25 <andylo25@163.com>
2016-10-11 21:02:05 +0800

    删除不必要信息

--- QiPaiServer/WebContent/admin/app/model/Notice.js
+++
@@ -1,8 +0,0 @@
-Ext.define('Bly.model.Notice', {
-    extend: 'Ext.data.Model',
-    fields: [
-	    {name: 'id', type: 'int'},
-	    {name: 'notice',type: 'string'},
-	    {name: 'flag',type: 'int'}
-    ]
-});
--- QiPaiServer/WebContent/admin/app/model/Sysconf.js
+++
@@ -1,8 +0,0 @@
-Ext.define('Bly.model.Sysconf', {
-    extend: 'Ext.data.Model',
-    fields: [
-    	{name: 'id', type: 'int'},
-	    {name: 'key',type: 'string'},
-	    {name: 'val',type: 'string'}
-    ]
-});
